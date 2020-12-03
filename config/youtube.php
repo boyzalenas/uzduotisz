0e85b62a7dfd6740c5a1f17d5d9da0c8b5cdd8c2
boyzalenas <75308798+boyzalenas@users.noreply.github.com>
2020-12-03 17:14:37 +0200

    LogIn to Youtube

---
+++ config/youtube.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_key' => env('YOUTUBE_API_KEY', 'AIzaSyCem8gSN87p5_mm7DK2e14umTFBt7b30YU'),
+    'oauth_client_id' => env('OAUTH2_CLIENT_ID', '712473785545-pldbd7prbdf5hvq4vaeahl1rmucifc4c.apps.googleusercontent.com'),
+    'oauth_client_secret' => env('OAUTH2_CLIENT_SECRET', 'WQRzQ5Catbtg_90wED66bFrm'),
+];
